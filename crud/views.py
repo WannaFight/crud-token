e6da698c4581b8aab919f16f2db53b89db67e2e4
WannaFight <itrushin99@gmail.com>
2021-05-20 12:11:03 +0300

    switch mixins based views

--- crud/views.py
+++ crud/views.py
@@ -1,90 +1,73 @@
 from typing import Union
 
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404
 from django.http import Http404
+from rest_framework import mixins, generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.status import (
-    HTTP_201_CREATED,
-    HTTP_204_NO_CONTENT,
-    HTTP_400_BAD_REQUEST,
-)
-from rest_framework.views import APIView
 
 from .serializers import UserSerializer
 
 
-class UserList(APIView):
+class UserList(mixins.ListModelMixin,
+               mixins.CreateModelMixin,
+               generics.GenericAPIView):
     """
     /api/v1/users/ endpoint.
     """
     permission_classes = [IsAuthenticated]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    def get(self, request) -> Response:
+    def get(self, request, *args, **kwargs):
         """
         Return every user from auth_user table without `write_only` fields.
         """
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
+        return self.list(request, *args, **kwargs)
 
-        return Response(serializer.data)
-
-    def post(self, request) -> Response:
+    def post(self, request, *args, **kwargs):
         """
         Create new user in auth_user table and return created user.
         """
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=HTTP_201_CREATED)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+        return self.create(request, *args, **kwargs)
 
 
-class UserDetail(APIView):
+class UserDetail(mixins.RetrieveModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.DestroyModelMixin,
+                 generics.GenericAPIView):
     """
     /api/v1/users/{int} endpoint
     [GET, PUT, PATCH, DELETE]
     """
     permission_classes = [IsAuthenticated]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    def get(self, request, pk: int) -> Union[Response, Http404]:
+    def get(self, request, *args, **kwargs) -> Union[Response, Http404]:
         """
         Return single user with id==`pk` or 404.
         """
-        user = get_object_or_404(User, pk=pk)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, pk: int) -> Union[Response, Http404]:
+    def put(self, request, *args, **kwargs) -> Union[Response, Http404]:
         """
         Update fields for user with id==`pk` and return a new one.
 
-        Have to pass `username` and `password`
+        Have to pass `username`, `password` and `is_active`
         even though they won't be changed.
         """
-        user = get_object_or_404(User, pk=pk)
-        serializer = UserSerializer(user, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+        return self.update(request, *args, **kwargs)
 
-    def patch(self, request, pk: int) -> Union[Response, Http404]:
+    def patch(self, request, *args, **kwargs):
         """
         Partially update fields for user with id==`pk`.
         """
-        user = get_object_or_404(User, pk=pk)
-        serializer = UserSerializer(user, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
 
-    def delete(self, request, pk: int) -> Union[Response, Http404]:
+    def delete(self, request, *args, **kwargs):
         """
         Delete user with id==`pk`.
         """
-        user = get_object_or_404(User, pk=pk)
-        user.delete()
-        return Response(status=HTTP_204_NO_CONTENT)
+        return self.destroy(request, *args, **kwargs)
